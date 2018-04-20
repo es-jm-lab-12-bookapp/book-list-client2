368f9dde1498eb6e5b3c77276e8beefc5e54d8ca
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-04-19 21:21:58 -0700

    removed a line of code

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -27,10 +27,6 @@ ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
   Book.all = [];
   Book.loadAll = rows => Book.all = rows.sort((a, b) => b.title - a.title).map(book => new Book(book));
 
-  Book.loadOne = book => console.log(book);
-
-
-
   Book.fetchAll = callback =>
     $.get(`${ENV.apiUrl}/api/v1/books`)
       .then(Book.loadAll)
