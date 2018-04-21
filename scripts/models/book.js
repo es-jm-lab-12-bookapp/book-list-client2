21b7624cf5880766cdc23c95fc92d6019febcf3a
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-04-21 15:07:14 -0700

    Fixed typo

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -5,7 +5,7 @@ var app = app || {};
 const ENV = {};
 
 ENV.isProduction = window.location.protocol === 'https:';
-ENV.productionApiUrl = 'http://es-jm-booklist.herokuapp.com';
+ENV.productionApiUrl = 'https://es-jm-booklist.herokuapp.com';
 ENV.developmentApiUrl = 'http://localhost:3000';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 
